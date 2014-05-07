89e0773c6d60b9c9a1bf1e1fddb48b7f5282c94f
Breno Teixeira <brenotx@gmail.com>
2014-05-07 16:51:41 -0300

    Refactoring SMTP config to use system environs

--- brenotx/settings.py
+++ brenotx/settings.py
@@ -132,9 +132,9 @@ CKEDITOR_CONFIGS = {
 }
 
 # SMTP configuration
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'my@gmail.com'
-EMAIL_HOST_PASSWORD = 'mypassword'
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
 
+EMAIL_HOST = os.environ.get('EMAIL_HOST', '')
+EMAIL_PORT = os.environ.get('EMAIL_PORT', '')
+EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER', '')
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD', '')
+EMAIL_USE_TLS = True
